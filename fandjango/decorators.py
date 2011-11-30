c5f0609baad3269cee0dd3d1573de09dfa40b491
Bulkan Evcimen <bulkan@gmail.com>
2011-11-30 15:53:11 +1100

    Redirect to proxy then to page tab

--- fandjango/decorators.py
+++ fandjango/decorators.py
@@ -1,9 +1,11 @@
 from functools import wraps
 
-from django.http import HttpResponse
+from django.http import HttpResponse, QueryDict
 from django.core.urlresolvers import reverse
 from django.core.handlers.wsgi import WSGIRequest
 from django.conf import settings
+
+from urllib import urlencode
 
 from utils import redirect_to_facebook_authorization
 
@@ -24,7 +26,17 @@ def facebook_authorization_required(redirect_uri=False, app_data=''):
             page_tab = None
             if not request.facebook or not request.facebook.user:
                 # redirect to app page_tab and use app_data
-                page_tab = request.facebook.page.url + "?sk=app_%s&app_data=%s" % (settings.FACEBOOK_APPLICATION_ID, request.get_full_path())
+                
+
+
+                #page_tab = "%s?app_data=%s" %(settings.FACEBOOK_APPLICATION_REDIRECT_URI, ';'.join([page_tab, request.get_full_path()]))
+
+                app_data = "%s?sk=app_%s;%s" %(request.facebook.page.url, settings.FACEBOOK_APPLICATION_ID, request.get_full_path())
+                page_tab = '%s?app_data=%s' %(settings.FACEBOOK_APPLICATION_REDIRECT_URI,app_data)
+
+
+
+                #page_tab = request.facebook.page.url + "?sk=app_%s&app_data=%s" % (settings.FACEBOOK_APPLICATION_ID, request.get_full_path())
 
                 return redirect_to_facebook_authorization(
                         redirect_uri = redirect_uri or page_tab or settings.FACEBOOK_APPLICATION_URL + request.get_full_path(),
