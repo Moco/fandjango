ecf5dc2aadf4f11000465f4b6bfaf6ddc587ebf0
Bulkan Evcimen <bulkan@gmail.com>
2011-12-13 11:31:38 +1100

    commented out the code to redirect to page tab

--- fandjango/decorators.py
+++ fandjango/decorators.py
@@ -13,7 +13,7 @@ def facebook_authorization_required(redirect_uri=False, app_data=''):
     """
     Redirect Facebook canvas views to authorization if required.
     
-    Arguments:
+    Arguments
     redirect_uri -- A string describing an URI to redirect to after authorization is complete.
                     Defaults to current URI in Facebook canvas (ex. http://apps.facebook.com/myapp/path/).
     """
@@ -23,7 +23,7 @@ def facebook_authorization_required(redirect_uri=False, app_data=''):
             
             request = [arg for arg in args if arg.__class__ is WSGIRequest][0]
             
-            page_tab = None
+            #page_tab = None
             if not request.facebook or not request.facebook.user:
                 # redirect to app page_tab and use app_data
                 
@@ -31,15 +31,17 @@ def facebook_authorization_required(redirect_uri=False, app_data=''):
 
                 #page_tab = "%s?app_data=%s" %(settings.FACEBOOK_APPLICATION_REDIRECT_URI, ';'.join([page_tab, request.get_full_path()]))
 
-                app_data = "%s?sk=app_%s;%s" %(request.facebook.page.url, settings.FACEBOOK_APPLICATION_ID, request.get_full_path())
-                page_tab = '%s?app_data=%s' %(settings.FACEBOOK_APPLICATION_REDIRECT_URI,app_data)
+                #app_data = "%s?sk=app_%s;%s" %(request.facebook.page.url, settings.FACEBOOK_APPLICATION_ID, request.get_full_path())
 
+                ## redirect to proxy url 
+                #page_tab = '%s?app_data=%s' %(settings.FACEBOOK_APPLICATION_REDIRECT_URI, app_data)
 
 
                 #page_tab = request.facebook.page.url + "?sk=app_%s&app_data=%s" % (settings.FACEBOOK_APPLICATION_ID, request.get_full_path())
+                #page_tab = None
 
                 return redirect_to_facebook_authorization(
-                        redirect_uri = redirect_uri or page_tab or settings.FACEBOOK_APPLICATION_URL + request.get_full_path(),
+                        redirect_uri = redirect_uri or settings.FACEBOOK_APPLICATION_URL + request.get_full_path(),
                         )
             return function(*args, **kwargs)
         return wrapper
