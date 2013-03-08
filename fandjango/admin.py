bc8b8a3fe0ce1c6a1333b237054a571548e0bd0a
Bulkan Evcimen <bulkan@gmail.com>
2013-03-08 05:10:36 +0000

    show Like & Friend in admin

--- fandjango/admin.py
+++ fandjango/admin.py
@@ -1,11 +1,32 @@
 from django.contrib import admin
-from models import User, OAuthToken
+from models import User, OAuthToken, Like, Friend
 
 class UserAdmin(admin.ModelAdmin):
     list_display = ['__unicode__', 'facebook_id', 'authorized', 'created_at', 'last_seen_at']
-    
+
 class OAuthTokenAdmin(admin.ModelAdmin):
     list_display = ['user', 'issued_at', 'expires_at', 'expired']
-    
+
 admin.site.register(User, UserAdmin)
 admin.site.register(OAuthToken, OAuthTokenAdmin)
+
+
+class FriendAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+        'friend_of',
+        'facebook_id'
+    ]
+
+admin.site.register(Friend, FriendAdmin)
+
+
+class LikeAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+        'category',
+        'like_id',
+        'facebook_user'
+    ]
+
+admin.site.register(Like, LikeAdmin)
